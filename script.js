83c023131e73f8badaa449041af9228c6d14fd6e
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-24 09:27:59 +0000

    Add turns and winner condition

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const gameManager = (function () {
     let winner;
 
-    const startGame = () => {
+    let x = 0;
+
+    let currentPlayer;
+
+    const setWinner = (player) => setWinner = player;
+
+    const playGame = () => {
         for (let x = 1; x <= 2; x++) {
             let _player = prompt(`Player ${x}, Enter Name`);
             playerManager.addPlayer(Player(_player));
         }
+
+        currentPlayer = playerManager.getPlayer(1).getName();
+
+        while (!winner && x <= 9) {
+            let input = prompt(`${currentPlayer}, Take Your Turn`);
+            boardManager.takeTurn(currentPlayer, input);
+            winner = boardManager.checkWinner();
+            if (currentPlayer === playerManager.getPlayer(1).getName()) {
+                currentPlayer = playerManager.getPlayer(2).getName();
+            } else {
+                currentPlayer = playerManager.getPlayer(1).getName();
+            }
+            x++;
+        }
+        x = 0;
+
+        //if winner is found
+        winner ? console.log(`Winner is ${winner}`) : console.log(`Tie`);
     }
 
     return {
-        winner,
-        startGame,
+        playGame,
+        setWinner,
     }
 })();
 
 const boardManager = (function () {
     let board = [
-        `A1`, `A2`, `A3`,
-        `B1`, `B2`, `B3`,
-        `C1`, `C2`, `C3`
+        [`A1`, `A2`, `A3`],
+        [`B1`, `B2`, `B3`],
+        [`C1`, `C2`, `C3`],
     ];
 
     const getBoard = () => board;
@@ -31,17 +55,48 @@ const boardManager = (function () {
         }
     }
 
-    const takeTurn = (player, input) => {
-        player = player.toLowerCase();
-        player === `x` || player.toLowerCase === `o` ?
-            boardManager.board[board.indexOf(input)] = player :
-            console.error(`Incorrect Input, Try Again`);
+    const takeTurn = (player, choice) => {
+        choice = choice.toUpperCase();
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < board[x].length; y++) {
+                if (board[x][y] === choice) {
+                    board[x][y] = player;
+                    console.log(`Placing ${player} at ${x}, ${y}`);
+                }
+            }
+        }
     };
+
+    const checkWinner = (player) => {
+        // check rows
+        for (let x = 0; x < 3; x++) {
+            if (board[0][x] === board[1][x] && board[1][x] === board[2][x]) {
+                return board[0][x];
+            }
+        }
+
+        //check columns
+        for (let x = 0; x < 3; x++) {
+            if (board[x][0] === board[x][1] && board[x][1] === board[x][2]) {
+                return board[x][0];
+            }
+        }
+
+        //check diagonals
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            return board[0][0];
+        } else if (board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
+            return board[2][0];
+        }
+
+        return false;
+    }
 
     return {
         displayBoard,
         takeTurn,
         getBoard,
+        checkWinner,
     }
 })();
 
@@ -82,10 +137,13 @@ const Player = function (name) {
 
     const resetScore = () => score = 0;
 
+    const getName = () => name;
+
     return {
         displayInfo,
         addScore,
         getScore,
         resetScore,
+        getName,
     }
 }
